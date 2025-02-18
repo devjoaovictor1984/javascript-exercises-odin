b0c7ab3ebba68f8c3af68509c7fced3b23b8274f
devjoaovictor1984 <192292865+devjoaovictor1984@users.noreply.github.com>
2025-02-18 19:19:59 -0400

    resolução de exercícios

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = ''; //inicializa uma string vazia  para armazenar o resultado
+
+    if (num < 0) return 'ERROR'; // Se num for negativo, retorna 'ERROR'
+
+    for (let i = 0; i < num; i++) { // Repete a string 'num' vezes
+        
+        result += string;
+    }
+
+    return result; // Retorna a string concatenada
 
 };
 
